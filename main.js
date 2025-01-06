7d2429f13f2a215f50fe8e997915548cff15be7b
unknown <you@example.com>
2025-01-06 21:16:32 +0700

    Làm scroll Fixed cho Header

--- main.js
+++ main.js
@@ -1,15 +1,38 @@
-// // Focus form
-let inputForm = document.querySelectorAll(".contact__input");
-
-inputForm.forEach((element) => {
-    element.addEventListener("input", () => {
-        if (element.value.length > 0) {
-            element.style.outline = "2px solid #1b1919";
-        }
-        element.addEventListener("blur", () => {
-            element.style.outline = "none";
+// Header Fixed
+function handleHeader() {
+    document.addEventListener("DOMContentLoaded", () => {
+        const headerFixed = document.querySelector(".header-fixed");
+        const headerHeight = document.querySelector(".header__container");
+        const logo = document.querySelector(".logo");
+        window.addEventListener("scroll", () => {
+            if (scrollY > 0) {
+                headerFixed.classList.add("scrolled");
+                headerHeight.classList.add("header__height");
+                logo.style.width = "198px";
+            } else {
+                headerFixed.classList.remove("scrolled");
+                headerHeight.classList.remove("header__height");
+                logo.style.width = "148px";
+            }
         });
     });
-});
+}
 
+// Focus form
+function handleFocusForm() {
+    let inputForm = document.querySelectorAll(".contact__input");
 
+    inputForm.forEach((element) => {
+        element.addEventListener("input", () => {
+            if (element.value.length > 0) {
+                element.style.outline = "2px solid #1b1919";
+            }
+            element.addEventListener("blur", () => {
+                element.style.outline = "none";
+            });
+        });
+    });
+}
+
+handleHeader();
+handleFocusForm()
