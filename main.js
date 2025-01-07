8cffb98b749ebbdf433376f83f36172ad45b7ce5
unknown <you@example.com>
2025-01-07 22:41:04 +0700

    Làm Menu-Drawer và thêm scrool behaviour

--- main.js
+++ main.js
@@ -4,27 +4,52 @@ function handleHeader() {
         const headerFixed = document.querySelector(".header-fixed");
         const headerHeight = document.querySelector(".header__container");
         const logo = document.querySelector(".logo");
+        const bars = document.querySelector(".bars");
+        const menuDrawer = document.querySelector(".menu-drawer");
         window.addEventListener("scroll", () => {
             if (scrollY > 0) {
                 headerFixed.classList.add("scrolled");
                 headerHeight.classList.add("header__height");
                 logo.style.width = "198px";
+                bars.classList.add("scrolled");
+                menuDrawer.style.padding = "4px";
             } else {
                 headerFixed.classList.remove("scrolled");
                 headerHeight.classList.remove("header__height");
                 logo.style.width = "148px";
+                bars.classList.remove("scrolled");
+                menuDrawer.style.padding = "20px";
             }
         });
     });
 }
 
+// Menu-drawer
+function handleOpenCloseDrawer() {
+    const clickBars = document.querySelector(".bars");
+    const drawerMenu = document.querySelector(".menu-drawer");
+    const closeDrawer = document.querySelector(".menu-close");
+    const overlay = document.querySelector(".overlay");
+
+    clickBars.addEventListener('click', ()=>{
+        drawerMenu.style.transform = "translateY(0%)";
+        overlay.style.visibility = "visible";
+    })
+
+    closeDrawer.onclick = function() {
+        drawerMenu.style.transform = "translateY(-100%)";
+        overlay.style.visibility = "hidden";
+    }
+
+}
+
 // Focus form
 function handleFocusForm() {
-    let inputForm = document.querySelectorAll(".contact__input");
+    const inputForm = document.querySelectorAll(".contact__input");
 
     inputForm.forEach((element) => {
         element.addEventListener("input", () => {
-            if (element.value.length > 0) {
+            if(element.value.length > 0) {
                 element.style.outline = "2px solid #1b1919";
             }
             element.addEventListener("blur", () => {
@@ -35,4 +60,5 @@ function handleFocusForm() {
 }
 
 handleHeader();
-handleFocusForm()
+handleOpenCloseDrawer();
+handleFocusForm();
