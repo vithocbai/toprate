f8c29a4fb39ea5b6fbb6d21be0576138e3b9dd54
unknown <you@example.com>
2025-01-08 14:53:28 +0700

    Làm BackTop cho website

--- main.js
+++ main.js
@@ -59,6 +59,29 @@ function handleFocusForm() {
     });
 }
 
+// Back Top
+const gotoTop = document.getElementById("gotoTop");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        document.getElementById("gotoTop").style.display = "block";
+    } else {
+        document.getElementById("gotoTop").style.display = "none";
+    }
+   
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+     $('html, body').animate({scrollTop:0}, 'slow');
+}
+
+gotoTop.onclick = function() {
+    topFunction()
+}
+
+
 handleHeader();
 handleOpenCloseDrawer();
 handleFocusForm();
